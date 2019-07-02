694e1b0a461eea3cd9f03ee9a68a8c386e691dbb
優樹永井 <wadachi0505@gmail.com>
2019-07-02 09:56:07 +0900

    Initial Commit

---
+++ SliderSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliderSample
+//
+//  Created by 優樹永井 on 2019/07/02.
+//  Copyright © 2019 com.nagai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
